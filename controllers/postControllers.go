9f8c48ce77a4f7feec4ab9b9c964caeec52c5673
andrekruss <30608106+andrekruss@users.noreply.github.com>
2023-05-02 00:14:13 -0300

    Post Request and Viper package
    Added database access to POST request plus viper package for config file

--- controllers/postControllers.go
+++ controllers/postControllers.go
@@ -1,12 +1,18 @@
 package controllers
 
 import (
+	"call-ms-users/database"
 	"call-ms-users/models"
+	"fmt"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func PostUser(ctx *fiber.Ctx) error {
+
+	collection := database.DBConn.Database("call-users").Collection("users")
+
 	// creating a dummy user
 	user := models.User{
 		Id:       1,
@@ -14,6 +20,14 @@ func PostUser(ctx *fiber.Ctx) error {
 		Password: "123",
 		UserType: "admin",
 		IsActive: true,
+	}
+
+	res, err := collection.InsertOne(ctx.Context(), user)
+
+	fmt.Printf("Inserted id: %v", res.InsertedID)
+
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	return ctx.Status(fiber.StatusCreated).JSON(user)
--- main.go
+++ main.go
@@ -2,16 +2,63 @@ package main
 
 import (
 	"call-ms-users/controllers"
+	"call-ms-users/database"
+	"context"
+	"fmt"
+	"log"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/spf13/viper"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+func readConfigFile() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		panic(fmt.Errorf("%s", err))
+	}
+}
+
+func createMongoClient(connectionString string) *mongo.Client {
+	client, err := mongo.NewClient(options.Client().ApplyURI(connectionString))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return client
+}
+
+func connectMongo(client *mongo.Client) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err := client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func routesSetup(app *fiber.App) {
+	app.Get("/", controllers.GetRoot)
+	app.Post("/create-user", controllers.PostUser)
+}
 
 func main() {
 
+	// sets env variables through viper package
+	readConfigFile()
+
+	// connecting to mongoDB
+	database.DBConn = createMongoClient(viper.GetString("database.connectionstring"))
+	connectMongo(database.DBConn)
+
+	// fiber app
 	app := fiber.New()
-
-	app.Get("/", controllers.GetRoot)
-	app.Post("/create-user", controllers.PostUser)
-
-	app.Listen(":3000")
+	routesSetup(app)
+	app.Listen(fmt.Sprintf(":%v", viper.GetString("app.port")))
 }
